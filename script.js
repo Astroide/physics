b5ce8eff5edf57daf8fdc1863680cb84d860df64
Olie Auger <olie.auger@gmail.com>
2021-11-17 16:04:34 -0500

    Add slow motion

--- script.js
+++ script.js
@@ -20,18 +20,41 @@ class Vector {
         }
     }
 
+    get angle() {
+        return Math.atan2(this.y, this.x);
+    }
+
+    set angle(angle) {
+        let magnitude = this.magnitude;
+        this.x = Math.cos(angle) * magnitude;
+        this.y = Math.sin(angle) * magnitude;
+    }
+
     normalize() {
         let magnitude = this.magnitude;
         this.x /= magnitude;
         this.y /= magnitude;
+        return this;
     }
 
     add(other) {
-        return new Vector(this.x + other.x, this.y + other.y);
+        this.x += other.x;
+        this.y += other.y;
+        return this;
     }
 
     sub(other) {
         return new Vector(this.x - other.x, this.y - other.y);
+    }
+
+    mul(val) {
+        this.x *= val;
+        this.y *= val;
+        return this;
+    }
+
+    clone() {
+        return new Vector(this.x, this.y);
     }
 }
 
@@ -42,17 +65,19 @@ class Thing {
         this.mass = mass;
         this.velocity = new Vector(0, 0);
         this.id = Math.floor(Math.random() * 100000);
-        this.color = 0;
-        while (this.color < 50) {
-            this.color = Math.floor(Math.random() * 0xFFFFFF);
-        }
-        this.radius = Math.sqrt((this.mass / 100000) / Math.PI);
+        this.color = Math.floor(Math.random() * 360);
+        this.gracePeriods = {
+
+        };
+    }
+
+    get radius() {
+        return Math.sqrt((this.mass / 100000) / Math.PI);
     }
 
     draw(ctx) {
-        this.radius = Math.sqrt((this.mass / 100000) / Math.PI);
         // ctx.fillStyle = this.mass == 10 ? 'blue' : 'red';
-        ctx.fillStyle = '#' + this.color.toString(16).padStart('0');
+        ctx.fillStyle = `hsl(${this.color}deg, 100%, 50%)`;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.fixed ? 10 : this.radius, 0, 2 * Math.PI);
         ctx.fill();
@@ -61,7 +86,7 @@ class Thing {
         ctx.strokeStyle = 'limegreen';
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + Math.cos(angle) * this.velocity.magnitude / 2e6, this.y + Math.sin(angle) * this.velocity.magnitude / 2e6);
+        ctx.lineTo(this.x + Math.cos(angle) * this.velocity.magnitude * 20, this.y + Math.sin(angle) * this.velocity.magnitude * 20);
         ctx.stroke();
     }
 
@@ -71,22 +96,24 @@ class Thing {
         t.id = this.id;
         t.fixed = this.fixed;
         t.color = this.color;
+        t.gracePeriods = this.gracePeriods;
         return t;
     }
 }
 
 let bodies = [
-    // new Thing(250, 250, 5.972e6),
+    // new Thing(300, 300, 5.972e6),
 ];
 // bodies[0].fixed = true;
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const slowMotionInput = document.querySelector('#slow-motion');
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 for (let i = 0; i < 45; i++) {
     let b = new Thing(Math.random() * canvas.width, Math.random() * canvas.height, 1000000);
-    const angle = Math.atan2(250 - b.y, 250 - b.x) + Math.PI / 2;
+    const angle = Math.atan2(300 - b.y, 300 - b.x) + Math.PI / 2;
     b.velocity = new Vector(Math.cos(angle) / 2, Math.sin(angle) / 2);
     // bodies.push(b);
 }
@@ -113,70 +140,90 @@ let startX = 0;
 let startY = 0;
 
 addEventListener('mousedown', e => {
-    startX = e.pageX;
-    startY = e.pageY;
-    mousePressed = true;
+    if (e.pageX < canvas.width && e.pageY < canvas.height) {
+        startX = e.pageX;
+        startY = e.pageY;
+        mousePressed = true;
+    }
 });
 
 addEventListener('mouseup', e => {
-    mousePressed = false;
-    if (x) {
-        let vec = new Vector(startX - e.pageX, startY - e.pageY);
-        vec.normalize();
-        vec.x *= -20000 * (distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY)) / 75);
-        vec.y *= -20000 * (distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY)) / 75);
-        for (let i = 0; i < 10; i++) {
-            let b = new Thing(startX + Math.random() * 50 - 25, startY + Math.random() * 50 - 25, 20000);
-            b.velocity = new Vector(vec.x, vec.y);
-            bodies.push(b);
+    if (e.pageX < canvas.width && e.pageY < canvas.height) {
+
+        mousePressed = false;
+        if (x) {
+            let vec = new Vector(startX - e.pageX, startY - e.pageY);
+            vec.normalize();
+            vec.x *= -1 * (distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY)) / 75);
+            vec.y *= -1 * (distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY)) / 75);
+            for (let i = 0; i < 10; i++) {
+                let b = new Thing(startX + Math.random() * 50 - 25, startY + Math.random() * 50 - 25, 1000000);
+                b.velocity = new Vector(vec.x, vec.y);
+                b.velocity.x *= 10;
+                b.velocity.y *= 10;
+                bodies.push(b);
+            }
+        } else {
+            let size = distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY));
+            size *= size;
+            size *= Math.PI;
+            size *= 100000;
+            let body = new Thing(startX, startY, size);
+            bodies.push(body);
+            let vec = new Vector(body.x - e.pageX, body.y - e.pageY);
+            vec.normalize();
+            vec.x *= -1;
+            vec.y *= -1;
+            if (e.shiftKey) {
+                vec.x *= 2;
+                vec.y *= 2;
+            }
+            if (e.metaKey) {
+                vec.x *= 2;
+                vec.y *= 2;
+            }
+            if (a) vec = new Vector(0, 0);
+            if (e.altKey) {
+                body.fixed = true;
+            }
+            body.velocity = vec;
         }
-    } else {
-        let size = distance(new Vector(startX, startY), new Vector(e.pageX, e.pageY));
-        size *= size;
-        size *= Math.PI;
-        size *= 100000;
-        let body = new Thing(startX, startY, size);
-        bodies.push(body);
-        let vec = new Vector(body.x - e.pageX, body.y - e.pageY);
-        vec.normalize();
-        vec.x *= -body.mass;
-        vec.y *= -body.mass;
-        if (e.shiftKey) {
-            vec.x *= 2;
-            vec.y *= 2;
-        }
-        if (e.metaKey) {
-            vec.x *= 2;
-            vec.y *= 2;
-        }
-        if (e.altKey) {
-            body.fixed = true;
-        }
-        body.velocity = vec;
     }
 });
 
 let mouseX = 0;
 let mouseY = 0;
-let meta, alt, shift, x;
+let meta, alt, shift, x, a, slowMotion;
 addEventListener('mousemove', e => {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-    meta = e.metaKey;
-    alt = e.altKey;
-    shift = e.shiftKey;
-    // bodies[0].x = mouseX;
-    // bodies[0].y = mouseY;
+    if (e.pageX < canvas.width && e.pageY < canvas.height) {
+
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        meta = e.metaKey;
+        alt = e.altKey;
+        shift = e.shiftKey;
+        // bodies[0].x = mouseX;
+        // bodies[0].y = mouseY;
+    }
 });
 addEventListener('keydown', e => {
     console.log(e.key);
     if (e.key.toLowerCase() == 'x') {
         x = true;
     }
+    if (e.key.toLowerCase() == 'a') {
+        a = true;
+    }
+    if (e.key.toLowerCase() == 's') {
+        slowMotion = !slowMotion;
+    }
 });
 addEventListener('keyup', e => {
     if (e.key.toLowerCase() == 'x') {
         x = false;
+    }
+    if (e.key.toLowerCase() == 'a') {
+        a = false;
     }
 });
 let fps = 60;
@@ -190,6 +237,9 @@ addEventListener('keydown', e => {
         trail = !trail;
     }
 });
+function interpolate(a, b, x) {
+    return a + (b - a) * x;
+}
 function main() {
     // if (bodies.length < 2) {
     // return;
@@ -202,18 +252,28 @@ function main() {
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     const newBodies = bodies.map(x => x.clone());
     let dead = [];
-    newBodies.forEach(body => {
+    let newObjects = [];
+    newBodies.forEach((body, index) => {
         if (paused) return;
         if (dead.includes(body.id)) {
             return;
         }
+        for (let id in body.gracePeriods) {
+            if (body.gracePeriods.hasOwnProperty(id)) {
+                body.gracePeriods[id] -= slowMotion ? (1 / slowMotionInput.value) : 1;
+                if (body.gracePeriods[id] <= 0) {
+                    delete body.gracePeriods[id];
+                }
+            }
+        }
+        let oldBody = bodies[index];
         if (!body.fixed) {
-            body.x += body.velocity.x / body.mass;
-            body.y += body.velocity.y / body.mass;
+            body.x += body.velocity.x / (slowMotion ? slowMotionInput.value : 1);
+            body.y += body.velocity.y / (slowMotion ? slowMotionInput.value : 1);
         }
         //console.log(">" + body.velocity.x);
         bodies.forEach(other => {
-            if (other.id == body.id || dead.includes(other.id)) {
+            if (other.id == body.id || dead.includes(other.id) || ((body.gracePeriods[other.id] !== undefined && (body.gracePeriods[other.id] > 0)) || ((other.gracePeriods[body.id] !== undefined && (other.gracePeriods[body.id] > 0))))) {
                 // //console.log('dupe');
                 return;
             }
@@ -224,21 +284,56 @@ function main() {
             //console.log(force);
             // force *= this.mass / other.mass;
             force /= 50;
-            dir.magnitude = force;
+            force /= body.mass;
+            dir.magnitude = force / (slowMotion ? slowMotionInput.value : 1);
             // console.log(dir.magnitude);
             body.velocity = body.velocity.add(dir);
-            if (distance(body, other) < (body.radius + other.radius)) {
-                if ((body.fixed || other.fixed) || body.mass < other.mass) {
+            if (distance(oldBody, other) < (oldBody.radius + other.radius) && (body.gracePeriods[other.id] === undefined || body.gracePeriods[other.id] <= 0)) {
+                if ((oldBody.fixed || other.fixed) || oldBody.mass < other.mass) {
                     // ...
                 } else {
-                    body.velocity.x *= body.mass;
-                    body.velocity.y *= body.mass;
+                    oldBody.velocity.x *= body.mass;
+                    oldBody.velocity.y *= body.mass;
                     other.velocity.x *= other.mass;
                     other.velocity.y *= other.mass;
-                    body.velocity.add(other.velocity);
+                    body.velocity = oldBody.velocity.add(other.velocity);
                     body.mass += other.mass;
                     body.velocity.x /= body.mass;
                     body.velocity.y /= body.mass;
+                    let centerOfMass = new Vector(oldBody.x * oldBody.mass, oldBody.y * oldBody.mass);
+                    centerOfMass.add(new Vector(other.x * other.mass, other.y * other.mass));
+                    centerOfMass.x /= oldBody.mass + other.mass;
+                    centerOfMass.y /= oldBody.mass + other.mass;
+                    body.x = centerOfMass.x;
+                    body.y = centerOfMass.y;
+                    let collisionSpeed = oldBody.velocity.sub(other.velocity).magnitude;
+                    let partMass = 1e6;
+                    if (collisionSpeed > 2 && (other.mass > 1e7 && oldBody.mass > 1e7)) {
+                        let extra = body.mass / 100 * (collisionSpeed / 1e9);
+                        body.mass -= extra;
+                        // let radius = body.radius;
+                        let count = extra / partMass;
+                        let newThings = [];
+                        for (let i = 0; i < count; i++) {
+                            let newObject = new Thing(0, 0, partMass);
+                            // console.log('size', radius, newObject.radius);
+                            newObject.x = body.x; //+ Math.cos(Math.PI * 2 * i / count) * (radius + newObject.radius + 10);
+                            newObject.y = body.y; //+ Math.sin(Math.PI * 2 * i / count) * (radius + newObject.radius + 10);
+                            newObject.velocity = body.velocity.clone().normalize().mul(Math.sign(Math.random() * 2 - 1)).mul(collisionSpeed / 1e9 * 10);
+                            newObject.velocity.add(body.velocity);
+                            // newObject.velocity = (new Vector(Math.cos(Math.PI * 2 * i / count + (Math.random() / 1.1 - 0.5 / 1.1)) * 5 * collisionSpeed / 1e9, Math.sin(Math.PI * 2 * i / count + (Math.random() / 1.1 - 0.5 / 1.1)) * 5 * collisionSpeed / 1e9)).mul(1 - (Math.random() - 0.5) / 1.5).mul(0.8);
+                            newObject.velocity.add(body.velocity);
+                            newObject.velocity.angle += Math.random() / 1.5 - 1 / 3;
+                            body.gracePeriods[newObject.id] = 100;
+                            for (let i = 0; i < newThings.length; i++) {
+                                const e = newThings[i];
+                                e.gracePeriods[newObject.id] = 100;
+                                newObject.gracePeriods[e.id] = 100;
+                            }
+                            newObjects.push(newObject);
+                            newThings.push(newObject);
+                        }
+                    }
                     dead.push(other.id);
                 }
             }
@@ -254,11 +349,12 @@ function main() {
         // } else if (body.y < 0) {
         // body.y = canvas.height - 1;
         // }
-        if (body.x ** 2 + body.y ** 2 > (canvas.width + 200) ** 2) {
-            body.dead = true;
-        }
+        // if (body.x ** 2 + body.y ** 2 > (canvas.width + 200) ** 2) {
+        // body.dead = true;
+        // }
     });
     bodies = newBodies;
+    bodies = bodies.concat(newObjects);
     bodies = bodies.filter(x => !dead.includes(x.id));
     bodies.forEach(body => {
         body.draw(ctx);
@@ -295,7 +391,7 @@ function main() {
     lastTime = Date.now();
     ctx.fillStyle = 'black';
     ctx.clearRect(20, 10, 80, 20);
-    ctx.fillText('FPS ' + Math.floor(fps) + (paused ? ' P' : ''), 20, 20);
+    ctx.fillText('FPS ' + Math.floor(fps) + (paused ? ' P' : '') + (slowMotion ? ' S ' + slowMotionInput.value : ''), 20, 20);
     if (fps < 30) {
         debugger;
     }
