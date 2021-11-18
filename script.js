c4261dea8d6d4a31b131ac8ab2eef3b9b166d799
Olie Auger <olie.auger@gmail.com>
2021-11-18 13:39:53 -0500

    Collision !

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ class Thing {
     }
 
     get radius() {
-        return Math.sqrt((this.mass / 100000) / Math.PI);
+        return this.fixed ? 10 : Math.sqrt((this.mass / 100000) / Math.PI);
     }
 
     draw(ctx) {
